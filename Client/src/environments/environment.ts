683f3ae8423414c9618db069d54e56f85a9d51f1
legendary614 <36903047+legendary614@users.noreply.github.com>
2018-08-21 22:08:39 +0300

    local env

--- Client/src/environments/environment.ts
+++ Client/src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   // api: 'https://backend.blurbiz.io',
   api: 'http://localhost:3040',
-  front_url: 'https://app.blurbiz.io/',
+  front_url: 'http://localhost:4200/',
   google_font_api: 'https://www.googleapis.com/webfonts/v1/webfonts',
   google_font_apiKey: 'AIzaSyDlrF-qcSga8p5YVjsTE-s_8oqwBhecvm4',
 };
