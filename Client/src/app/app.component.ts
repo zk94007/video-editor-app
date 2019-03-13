f2b7f3a55421379cadd3713fdaf0541c374a4557
John Pozy <john.pozy87@gmail.com>
2019-03-14 01:36:14 +0800

    fix: show loader until load event is complete

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
-  
+    constructor() {
+        const bodyEl: HTMLElement = document.getElementsByTagName('body')[0];
+
+        window.addEventListener('load', () => {
+            setTimeout(() => {
+                bodyEl.classList.add('loaded');
+            }, 100);
+        });
+    }
 }
