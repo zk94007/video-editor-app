b4883d83b8d86d814e2309782cc80d2dee7615f9
John Pozy <john.pozy87@gmail.com>
2019-03-22 02:43:50 +0800

    refactor: update caption studio functionality

--- Client/src/app/layout/caption-studio/caption-studio.component.ts
+++ Client/src/app/layout/caption-studio/caption-studio.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ChangeDetectorRef, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ChangeDetectorRef, ElementRef, Renderer2 } from '@angular/core';
 import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
 
 import * as moment from 'moment';
@@ -18,7 +18,7 @@ export class CaptionStudioComponent implements OnInit {
     public props = {
         video: {
             sources: [{
-                url: 'https://assets.mixkit.co/videos/99541/99541-720.mp4',
+                url: 'https://blurbizstagdiag910.blob.core.windows.net/stage/dec369e0-45b0-11e9-ae1d-9f9091396a20.mp4', // 'https://blurbizstagdiag910.blob.core.windows.net/stage/dec369e0-45b0-11e9-ae1d-9f9091396a20.mp4', // 'https://assets.mixkit.co/videos/99541/99541-720.mp4',
                 type: 'video/mp4'
             }],
             duration: null,
@@ -35,7 +35,8 @@ export class CaptionStudioComponent implements OnInit {
 
     constructor(
         private changeDetectorRef: ChangeDetectorRef,
-        private formBuilder: FormBuilder
+        private formBuilder: FormBuilder,
+        private renderer: Renderer2
     ) { }
 
     ngOnInit() {
@@ -47,7 +48,7 @@ export class CaptionStudioComponent implements OnInit {
 
         this.videoPlayer.api.getDefaultMedia().subscriptions.loadedData
             .subscribe(_ => {
-                this.addSubtitle(0, this.videoPlayer.api.duration);
+                this.addSubtitle(this.videoPlayer.api.duration);
             });
 
         this.videoPlayer.api.getDefaultMedia().subscriptions.timeUpdate
@@ -56,16 +57,41 @@ export class CaptionStudioComponent implements OnInit {
             });
     }
 
-    public toDuration(seconds = 0) {
-        return moment.utc(seconds * 1000).format('mm:ss');
+    public toMillisecond(seconds = 0) {
+        return moment.utc(seconds * 1000);
     }
 
-    public addSubtitle(start = 0, end = 60) {
+    public addSubtitle(totalTime) {
+        let startTime;
+        let endTime;
+
+        const multiplier = 5;
         const control = <FormArray>this.formSubtitle.controls['subtitles'];
+
+        startTime = control.value.length > 0 ? (control.value[control.value.length - 1].endTime) : 0;
+        endTime = control.value.length > 0 ? (control.value[control.value.length - 1].endTime + multiplier) : multiplier;
+
+        if (endTime > totalTime) {
+            startTime = 0;
+            endTime = multiplier;
+        }
+
         const addrCtrl = this.formBuilder.group({
-            text: 'test caption',
-            duration: [[start, end]]
+            text: null,
+            startTime: startTime,
+            endTime: endTime,
+            totalTime: totalTime,
+            rangeTime: [[startTime, endTime]],
+            resposition: {
+                left: null,
+                top: null,
+                width: null,
+                height: null
+            },
+            dataurl: null
         });
+
+        this.seekTime(endTime);
 
         control.push(addrCtrl);
     }
@@ -75,18 +101,17 @@ export class CaptionStudioComponent implements OnInit {
     }
 
     public seekTime(time) {
-        console.log(time);
-        // this.videoPlayer.api.seekTime(time);
+        this.videoPlayer.api.currentTime = time;
     }
 
-    public updateStyle(event, subtitle: HTMLElement, videoWrapper: HTMLElement) {
+    public updateStyle(event, subtitle: HTMLElement, videoWrapper) {
         const subtitleChild: any = subtitle.children[0];
 
         subtitle.style.fontFamily = event.font.family;
         subtitle.style.fontSize = `${event.font.size}px`;
         subtitle.style.fontWeight = event.font.weight;
         subtitle.style.fontStyle = event.font.style;
-        subtitle.style.color = event.font.color;
+        subtitle.style.color = event.font.color.hex;
         subtitle.style.textAlign = event.font.align;
         subtitle.style.alignSelf = event.caption.align;
         subtitle.style.padding = '10px';
@@ -98,18 +123,30 @@ export class CaptionStudioComponent implements OnInit {
         }
 
         if (videoWrapper) {
-            videoWrapper.classList.remove(`embed-responsive-1by1`);
-            videoWrapper.classList.remove(`embed-responsive-16by9`);
-            videoWrapper.classList.remove(`embed-responsive-9by16`);
-            videoWrapper.classList.add(`embed-responsive-${event.video.ratio}`);
-            videoWrapper.style.backgroundColor = event.video.color.hex;
+            const ratio = this._aspectRatio(event.video.ratio);
+
+            this.renderer.setStyle(videoWrapper, 'background-color', event.video.color.hex);
+
+            if (event.video.ratio === '16by9') {
+                this.renderer.removeStyle(videoWrapper, 'width');
+                this.renderer.setStyle(videoWrapper, 'height', `${ratio.ratioHeight}px`);
+            } else if (event.video.ratio === '1by1') {
+                this.renderer.setStyle(videoWrapper, 'height', `${ratio.ratioWidth}px`);
+                this.renderer.setStyle(videoWrapper, 'width', `${ratio.ratioWidth}px`);
+            } else if (event.video.ratio === '9by16') {
+                this.renderer.setStyle(videoWrapper, 'height', `${ratio.elementHeight}px`);
+                this.renderer.setStyle(videoWrapper, 'width', `${ratio.ratioWidth}px`);
+            }
         }
 
         switch (event.caption.type) {
             case 'none':
                 break;
             case 'outline':
-                subtitle.style.textShadow = `-1px -1px 0px ${event.caption.color.hex}, 1px -1px 0px ${event.caption.color.hex}, -1px 1px 0px ${event.caption.color.hex}, 1px 1px 0px ${event.caption.color.hex}`;
+                subtitle.style.textShadow = `-1px -1px 0px ${event.caption.color.hex},
+                                            1px -1px 0px ${event.caption.color.hex},
+                                            -1px 1px 0px ${event.caption.color.hex},
+                                            1px 1px 0px ${event.caption.color.hex}`;
                 break;
             case 'highlight':
                 subtitleChild.style.backgroundColor = event.caption.color.rgba;
@@ -120,5 +157,36 @@ export class CaptionStudioComponent implements OnInit {
             default:
                 break;
         }
+    }
+
+    private _aspectRatio(ratio) {
+        const height = 320;
+        const width = 428;
+        let newWidth;
+        let newHeight;
+
+        switch (ratio) {
+            case '16by9':
+                newWidth = ((1920 / 1080) * height);
+                newHeight = ((1080 / 1920) * width);
+                break;
+            case '9by16':
+                newWidth = ((1080 / 1920) * height);
+                newHeight = ((1920 / 1080) * width);
+                break;
+            case '1by1':
+                newWidth = ((400 / 400) * height);
+                newHeight = ((400 / 400) * width);
+                break;
+            default:
+                break;
+        }
+
+        return {
+            ratioWidth: newWidth,
+            ratioHeight: newHeight,
+            elementWidth: width,
+            elementHeight: height
+        };
     }
 }
